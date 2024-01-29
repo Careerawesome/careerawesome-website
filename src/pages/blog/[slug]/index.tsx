29eeb3537aea433c55a5977cc209d4431d98b2f2
Klight96 <klightkayode05@gmail.com>
2024-01-29 09:35:56 +0100

    static

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+const Post = ({ post }: any) => {
+  // We get the blog post and then render it 
+
+  return <div>Post</div>;
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const res = await fetch("https://api.careerawesome.com/api/posts");
+  const posts = await res.json();
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post: { slug: any }) => ({
+    params: { slug: post.slug },
+  }));
+  return { paths, fallback: false };
 }
 
-export default index
+export async function getStaticProps({ params }: any) {
+  const res = await fetch(`https://api.careerawesome.com/api/posts/${params.slug}`);
+  const post = await res.json();
+  return { props: { post } };
+}
---
+++ src/pages/blog/series/[name].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SeriesName() {
+  return (
+    <div>SeriesName</div>
+  )
+}
+
+export default SeriesName
